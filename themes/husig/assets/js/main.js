634d69012f04920a46f307bb7e19a74887104559
Dibyan Neupane <dibyanneupane@gmail.com>
2024-06-16 19:18:55 +0545

    Fixed features grid images and content

--- themes/husig/assets/js/main.js
+++ themes/husig/assets/js/main.js
@@ -89,3 +89,46 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 //-------E N D-----------
+
+// ---
+document.addEventListener('DOMContentLoaded', function () {
+  const scrollContainer = document.getElementById('slider-container');
+  const halfwayScroll = (scrollContainer.scrollWidth - scrollContainer.clientWidth) / 2;
+
+  // Set initial scroll position to halfway
+  scrollContainer.scrollLeft = halfwayScroll;
+
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  scrollContainer.addEventListener('mousedown', (e) => {
+    isDown = true;
+    scrollContainer.classList.add('active');
+    startX = e.pageX - scrollContainer.offsetLeft;
+    scrollLeft = scrollContainer.scrollLeft;
+    scrollContainer.style.cursor = 'grabbing';
+  });
+
+  scrollContainer.addEventListener('mouseleave', () => {
+    isDown = false;
+    scrollContainer.classList.remove('active');
+    scrollContainer.style.cursor = 'grab';
+  });
+
+  scrollContainer.addEventListener('mouseup', () => {
+    isDown = false;
+    scrollContainer.classList.remove('active');
+    scrollContainer.style.cursor = 'grab';
+  });
+
+  scrollContainer.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - scrollContainer.offsetLeft;
+    const walk = (x - startX) * 2; // Adjust the scroll speed
+    scrollContainer.scrollLeft = scrollLeft - walk;
+  });
+});
+
+// ---
