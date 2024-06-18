40b3675d06e5bfffc51a12e3f12cd76766d288a3
Dibyan Neupane <dibyanneupane@gmail.com>
2024-06-18 18:30:34 +0545

    Initialized platforms accordion UI

--- public/js/main.js
+++ public/js/main.js
@@ -123,4 +123,30 @@
       }
     };
   });
+  document.addEventListener("DOMContentLoaded", () => {
+    const uniqueAccordionHeaders = document.querySelectorAll(".unique-accordion-header");
+    uniqueAccordionHeaders.forEach((header) => {
+      header.addEventListener("click", () => {
+        const content = header.nextElementSibling;
+        const icon = header.querySelector(".unique-open-close");
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+          content.style.padding = "0 32px";
+          icon.classList.remove("rotate-icon");
+        } else {
+          document.querySelectorAll(".unique-accordion-content").forEach((c) => {
+            c.style.maxHeight = null;
+            c.style.padding = "0 32px";
+            const otherIcon = c.previousElementSibling.querySelector(".unique-open-close");
+            if (otherIcon) {
+              otherIcon.classList.remove("rotate-icon");
+            }
+          });
+          content.style.maxHeight = "62px";
+          content.style.padding = "8px 32px 22px 32px";
+          icon.classList.add("rotate-icon");
+        }
+      });
+    });
+  });
 })();
